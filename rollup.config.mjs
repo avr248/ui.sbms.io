7d6b14f307b7d21d5417fe37eccd2f956587e071
Avraham Avrahami <avr248@gmail.com>
2024-01-21 02:43:41 +0200

    update

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,18 +2,20 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import packageJson from "./package.json" assert { type: "json" };
 
 export default [
     {
         input: "index.ts",
         output: [
             {
-                file: "dist/cjs/index.js",
+                file: packageJson.main,
                 format: "cjs",
                 sourcemap: true,
             },
             {
-                file: "dist/esm/index.js",
+                file: packageJson.module,
                 format: "esm",
                 sourcemap: true,
             },
@@ -22,11 +24,13 @@ export default [
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
+            postcss(),
         ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+        external: [/\.css$/],
     },
 ];
